abe9f9cf3a76ca6951ac7ba254936978f1981129
stevejobsmyguru <hariharansankara@gmail.com>
2019-11-12 14:34:05 +0530

    Update Test_TSO_Calling_Main.R

--- Test_TSO_Calling_Main.R
+++ Test_TSO_Calling_Main.R
@@ -1,6 +1,6 @@
 library(xts)
 ColClasses = c( "character","numeric")
-Data <- read.zoo("~/MyOpenSource/AsyncTSOutliers/ML_Algo_TestArea/Kohl_BB_Data.csv", index.column = 1, sep = ",", header = TRUE, FUN = as.POSIXct, colClasses = ColClasses)
+Data <- read.zoo("./Test_Data.csv", index.column = 1, sep = ",", header = TRUE, FUN = as.POSIXct, colClasses = ColClasses)
 xts_data = as.xts(Data)
 ts0_object = DetectTSOutliers_UnSupervised(xts_data)
 print(ts0_object$anomalyPoints)
